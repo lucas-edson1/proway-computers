6ba563999c98c1f9b2c8cd99052f2000d129a361
Lucas <lucasedson03@gmail.com>
2024-01-04 10:30:34 -0300

    Auto-generated commit [ci skip]

---
+++ server/main.server.mjs
@@ -0,0 +1,2 @@
+import './polyfills.server.mjs';
+import{a}from"./chunk-2IU2ETYF.mjs";import"./chunk-KURGCJLN.mjs";import"./chunk-EURDVMU3.mjs";import"./chunk-BAGUUGS5.mjs";import"./chunk-CEVD6H3L.mjs";import"./chunk-5F5DFS4Z.mjs";import"./chunk-KRLCULJA.mjs";export{a as default};
